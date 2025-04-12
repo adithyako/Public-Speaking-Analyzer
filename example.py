f43a76e9922da6284ab3abed6f1292cc3dd1e10e
adithya <adithya@umd.edu>
2025-04-12 14:22:34 -0400

    Added Bad audio

--- example.py
+++ example.py
@@ -1,5 +1,9 @@
 import whisper
 
-model = whisper.load_model("small")
-result = model.transcribe("audio.m4a", temperature = 0.5)
+model = whisper.load_model("tiny")
+result = model.transcribe(
+    "bad.mp3", 
+    temperature = 0.4,
+    initial_prompt="Umm, let me think like, hmm... Okay, here's what I'm, like, thinking. So uhm, yeaah. Okay, ehm, uuuh."
+    )
 print(result["text"])
